33ddd81433180ac77f4949afff861bb839e2b15b
Kor-Maks <maksim57119@gmail.com>
2025-07-13 14:57:14 +0300

    Update main.js

--- Курс Aroken/wake-bake/js/main.js
+++ Курс Aroken/wake-bake/js/main.js
@@ -1,4 +1,4 @@
-(function() { // записываем переменные локально!
+(function() {
 
   // ----------САМОСТОЯТЕЛЬНАЯ РАБОТА ----------------
 
@@ -16,22 +16,21 @@ nav.addEventListener('click', () => {
 
 //--------------РАБОТА ПРЕПОДАВАТЕЛЯ--------------------
 
-  // // Используем делегирование событий!
-  // document.addEventListener('click', burgerInit);  // по клику на документ будет выполнятся функця (burgerInit)
+  // document.addEventListener('click', burgerInit);
 
-  // function burgerInit(e) { // создаём функцию аргументом которой является событие (e)
+  // function burgerInit(e) {
 
-  //   const burgerIcon = e.target.closest('.burger-icon'); // e.target проверяет есть ли у данного элемента родитель с классом (.burger-icon)
+  //   const burgerIcon = e.target.closest('.burger-icon');
   //   const burgerNavLink = e.target.closest('.nav__link');
 
-  //   if (!burgerIcon && !burgerNavLink) return; // если не !burgerIcon и !burgerNavLink тогда return (возврат), если что-то используется тогда выполняется код дальше!
+  //   if (!burgerIcon && !burgerNavLink) return;
 
-  //   if (document.documentElement.clientWidth > 900) return; // если ширина окна больше 900px то будет возврат(return) код дапльше не выполняется!
+  //   if (document.documentElement.clientWidth > 900) return;
 
-  //   if (!document.body.classList.contains('body--opened-menu')) { // если у .body нет класса .body--opened-menu то возвращается true и условие срабатывает
-  //     document.body.classList.add('body--opened-menu') // код выполняется и у body добавляется класс .body--opened-menu
+  //   if (!document.body.classList.contains('body--opened-menu')) {
+  //     document.body.classList.add('body--opened-menu')
   //   } else {
-  //     document.body.classList.remove('body--opened-menu') // в противном случае (если класс .body--opened-menu есть) он удаляется
+  //     document.body.classList.remove('body--opened-menu')
   //   }
   // }
 
