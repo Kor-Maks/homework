b29e977d7f3746b30091ac9b7855011f2ee692e7
Kor-Maks <maksim57119@gmail.com>
2025-07-14 16:32:14 +0300

    Add files via upload

--- Курс Aroken/wake-bake/js/main.js
+++ Курс Aroken/wake-bake/js/main.js
@@ -1,4 +1,4 @@
-(function() {
+(function() { // записываем переменные локально!
 
   // ----------САМОСТОЯТЕЛЬНАЯ РАБОТА ----------------
 
@@ -16,21 +16,22 @@ nav.addEventListener('click', () => {
 
 //--------------РАБОТА ПРЕПОДАВАТЕЛЯ--------------------
 
-  // document.addEventListener('click', burgerInit);
+  // // Используем делегирование событий!
+  // document.addEventListener('click', burgerInit);  // по клику на документ будет выполнятся функця (burgerInit)
 
-  // function burgerInit(e) {
+  // function burgerInit(e) { // создаём функцию аргументом которой является событие (e)
 
-  //   const burgerIcon = e.target.closest('.burger-icon');
+  //   const burgerIcon = e.target.closest('.burger-icon'); // e.target проверяет есть ли у данного элемента родитель с классом (.burger-icon)
   //   const burgerNavLink = e.target.closest('.nav__link');
 
-  //   if (!burgerIcon && !burgerNavLink) return;
+  //   if (!burgerIcon && !burgerNavLink) return; // если не !burgerIcon и !burgerNavLink тогда return (возврат), если что-то используется тогда выполняется код дальше!
 
-  //   if (document.documentElement.clientWidth > 900) return;
+  //   if (document.documentElement.clientWidth > 900) return; // если ширина окна больше 900px то будет возврат(return) код дапльше не выполняется!
 
-  //   if (!document.body.classList.contains('body--opened-menu')) {
-  //     document.body.classList.add('body--opened-menu')
+  //   if (!document.body.classList.contains('body--opened-menu')) { // если у .body нет класса .body--opened-menu то возвращается true и условие срабатывает
+  //     document.body.classList.add('body--opened-menu') // код выполняется и у body добавляется класс .body--opened-menu
   //   } else {
-  //     document.body.classList.remove('body--opened-menu')
+  //     document.body.classList.remove('body--opened-menu') // в противном случае (если класс .body--opened-menu есть) он удаляется
   //   }
   // }
 
